851e41acc243a3e94e76a37df634d680842cfeb0
Dyeizon <dyeizonque@gmail.com>
2023-11-11 23:55:03 -0300

    button color

--- src/[Dashboard]/[Analise]/Result/Fungi.jsx
+++ src/[Dashboard]/[Analise]/Result/Fungi.jsx
@@ -29,7 +29,7 @@ export default function Fungi({ img, name, cientificName, desc }) {
             <div className="flex-grow" />
     
             <button
-              onClick={() => handleButton()} className="analise-fungi-info-button inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => handleButton()} className="analise-fungi-info-button inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none focus:ring-pink-300"
             >
               Saiba mais
               <svg
