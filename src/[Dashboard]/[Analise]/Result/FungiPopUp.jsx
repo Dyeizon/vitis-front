85988a70e55b4032cd79853893bbc7675d21d2d9
dyeizon <dyeizonque@gmail.com>
2023-11-16 18:26:16 -0300

    modal width

--- src/[Dashboard]/[Analise]/Result/FungiPopUp.jsx
+++ src/[Dashboard]/[Analise]/Result/FungiPopUp.jsx
@@ -32,7 +32,7 @@ export default function FungiPopUp({dados, passChildState}) {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all w-5/6">
+            <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all w-11/12">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
                     <div className="mt-3 sm:ml-4 sm:mt-0 sm:text-left">
